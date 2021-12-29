dcf8db355873641c25b2b7456d1618247dfcdfb3
bigting84 <47157127+bigting84@users.noreply.github.com>
2021-12-29 01:07:00 -0800

    Add files via upload

--- resize_nii_256.sh
+++ resize_nii_256.sh
@@ -12,7 +12,7 @@ echo $CASE
 echo $DPATH 
 
 gunzip ${DPATH}/${CASE}.nii.gz
-binary/nii2mnc ${DPATH}/${CASE}.nii ${SPATH}/${CASE}.mnc
+binary/nii2mnc ${DPATH}/${CASE}.nii ${DPATH}/${CASE}.mnc
 
 binary/mincresample -trilinear -nelements 256 256 256 -start -100 -120 -90 -step 0.8 0.8 0.8 ${DPATH}/${CASE}.mnc ${SPATH}/${CASE}_ds.mnc -clobber
 
@@ -21,6 +21,7 @@ binary/mnc2nii ${SPATH}/${CASE}_ds.mnc ${SPATH}/${CASE}_ds.nii
 gzip ${SPATH}/${CASE}_ds.nii
 
 rm ${SPATH}/${CASE}_ds.mnc
+rm ${DPATH}/${CASE}.mnc
 
 done
 
